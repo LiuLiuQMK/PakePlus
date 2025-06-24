29344108cf063a9116e99fe19dc012c2047663a2
LiWen <108209252+LiuLiuQMK@users.noreply.github.com>
2025-06-24 20:03:39 +0800

    update src file from pakeplus

---
+++ src/emi_rx/output/iap_transport.d
@@ -0,0 +1,29 @@
+./output/iap_transport.o: src\IAP\IAP_Transport.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  src\IAP\IAP_Transport.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  src\IAP_BSP\bsp_usb_hid_iap.h sdk\src\StartUP\ing916\ingsoc.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\mpu_armv7.h \
+  sdk\src\StartUP\ing916\..\__ingsoc.h \
+  sdk\src\FWlib\peripheral_pinctrl.h sdk\src\FWlib\peripheral_adc.h \
+  sdk\src\FWlib\peripheral_comparator.h sdk\src\FWlib\peripheral_dma.h \
+  sdk\src\FWlib\peripheral_sysctrl.h sdk\src\FWlib\peripheral_efuse.h \
+  sdk\src\FWlib\peripheral_gpio.h sdk\src\FWlib\peripheral_i2c.h \
+  sdk\src\FWlib\peripheral_i2s.h sdk\src\FWlib\peripheral_ir.h \
+  sdk\src\FWlib\peripheral_pdm.h sdk\src\FWlib\peripheral_pte.h \
+  sdk\src\FWlib\peripheral_pwm.h sdk\src\FWlib\peripheral_qdec.h \
+  sdk\src\FWlib\peripheral_rtc.h sdk\src\FWlib\peripheral_ssp.h \
+  sdk\src\FWlib\peripheral_sysctrl.h sdk\src\FWlib\peripheral_timer.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h \
+  sdk\src\FWlib\peripheral_uart.h sdk\src\FWlib\peripheral_usb.h \
+  src\IAP\IAP_UserDef.h sdk\bundles\noos_mini\inc\platform_api.h \
+  sdk\bundles\noos_mini\inc\ll_api.h \
+  sdk\bundles\noos_mini\inc\btstack_util.h \
+  sdk\bundles\noos_mini\inc\bluetooth.h src\IAP\IAP_Application.h \
+  src\IAP\IAP_FLASH_MAP.H sdk\src\BSP\eflash.h src\IAP\IAP_UserDef.h
