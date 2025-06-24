dc222bde55f5edf946bb1f54e717dab27414f33d
LiWen <108209252+LiuLiuQMK@users.noreply.github.com>
2025-06-24 19:35:26 +0800

    update src file from pakeplus

---
+++ src/Ble_dongle/output/gatt_client_util.d
@@ -0,0 +1,34 @@
+./output/gatt_client_util.o: sdk\src\Tools\gatt_client_util.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  sdk\src\Tools\gatt_client_util.h sdk\bundles\typical\inc\gatt_client.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  sdk\bundles\typical\inc\bluetooth.h sdk\bundles\typical\inc\bt_types.h \
+  sdk\bundles\typical\inc\btstack_linked_list.h \
+  sdk\bundles\typical\inc\btstack_util.h \
+  sdk\bundles\typical\inc\btstack_event.h \
+  sdk\src\StartUP\ing916\compiler.h sdk\bundles\typical\inc\gap.h \
+  sdk\bundles\typical\inc\platform_api.h sdk\src\StartUP\ing916\ingsoc.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\mpu_armv7.h \
+  sdk\src\StartUP\ing916\..\__ingsoc.h \
+  sdk\src\FWlib\peripheral_pinctrl.h sdk\src\FWlib\peripheral_adc.h \
+  sdk\src\FWlib\peripheral_comparator.h sdk\src\FWlib\peripheral_dma.h \
+  sdk\src\FWlib\peripheral_sysctrl.h sdk\src\FWlib\peripheral_efuse.h \
+  sdk\src\FWlib\peripheral_gpio.h sdk\src\FWlib\peripheral_i2c.h \
+  sdk\src\FWlib\peripheral_i2s.h sdk\src\FWlib\peripheral_ir.h \
+  sdk\src\FWlib\peripheral_pdm.h sdk\src\FWlib\peripheral_pte.h \
+  sdk\src\FWlib\peripheral_pwm.h sdk\src\FWlib\peripheral_qdec.h \
+  sdk\src\FWlib\peripheral_rtc.h sdk\src\FWlib\peripheral_ssp.h \
+  sdk\src\FWlib\peripheral_sysctrl.h sdk\src\FWlib\peripheral_timer.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h \
+  sdk\src\FWlib\peripheral_uart.h sdk\src\FWlib\peripheral_usb.h \
+  sdk\bundles\typical\inc\ll_api.h sdk\bundles\typical\inc\att_db.h \
+  sdk\bundles\typical\inc\btstack_event.h \
+  sdk\bundles\typical\inc\btstack_defines.h sdk\src\Tools\sig_uuid.h
