379496fbcdfa2bb4270d53fee81bde5425006327
LiWen <108209252+LiuLiuQMK@users.noreply.github.com>
2025-06-24 19:37:35 +0800

    update src file from pakeplus

---
+++ src/Ble_dongle/output/profile.d
@@ -0,0 +1,53 @@
+./output/profile.o: src\profile.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  sdk\bundles\typical\inc\platform_api.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  sdk\src\StartUP\ing916\ingsoc.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\mpu_armv7.h \
+  sdk\src\StartUP\ing916\..\__ingsoc.h \
+  sdk\src\FWlib\peripheral_pinctrl.h sdk\src\FWlib\peripheral_adc.h \
+  sdk\src\FWlib\peripheral_comparator.h sdk\src\FWlib\peripheral_dma.h \
+  sdk\src\FWlib\peripheral_sysctrl.h sdk\src\FWlib\peripheral_efuse.h \
+  sdk\src\FWlib\peripheral_gpio.h sdk\src\FWlib\peripheral_i2c.h \
+  sdk\src\FWlib\peripheral_i2s.h sdk\src\FWlib\peripheral_ir.h \
+  sdk\src\FWlib\peripheral_pdm.h sdk\src\FWlib\peripheral_pte.h \
+  sdk\src\FWlib\peripheral_pwm.h sdk\src\FWlib\peripheral_qdec.h \
+  sdk\src\FWlib\peripheral_rtc.h sdk\src\FWlib\peripheral_ssp.h \
+  sdk\src\FWlib\peripheral_sysctrl.h sdk\src\FWlib\peripheral_timer.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h \
+  sdk\src\FWlib\peripheral_uart.h sdk\src\FWlib\peripheral_usb.h \
+  sdk\bundles\typical\inc\ll_api.h sdk\bundles\typical\inc\att_db.h \
+  sdk\bundles\typical\inc\bluetooth.h sdk\bundles\typical\inc\bt_types.h \
+  sdk\bundles\typical\inc\btstack_linked_list.h \
+  sdk\bundles\typical\inc\gap.h sdk\bundles\typical\inc\att_dispatch.h \
+  sdk\bundles\typical\inc\btstack_util.h \
+  sdk\bundles\typical\inc\btstack_util.h \
+  sdk\bundles\typical\inc\btstack_event.h \
+  sdk\src\StartUP\ing916\compiler.h sdk\bundles\typical\inc\gap.h \
+  sdk\bundles\typical\inc\btstack_defines.h \
+  sdk\bundles\typical\inc\gatt_client.h \
+  sdk\bundles\typical\inc\btstack_event.h \
+  sdk\bundles\typical\inc\os\FreeRTOS.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  sdk\bundles\typical\inc\os\FreeRTOSConfig.h \
+  sdk\bundles\typical\inc\os\projdefs.h \
+  sdk\bundles\typical\inc\os\portable.h \
+  sdk\bundles\typical\inc\os\deprecated_definitions.h \
+  sdk\bundles\typical\inc\os\portmacro.h \
+  sdk\bundles\typical\inc\os\.\portable\GCC\ARM_CM4F\portmacro.h \
+  sdk\bundles\typical\inc\os\mpu_wrappers.h \
+  sdk\bundles\typical\inc\os\semphr.h sdk\bundles\typical\inc\os\queue.h \
+  sdk\bundles\typical\inc\os\task.h sdk\bundles\typical\inc\os\list.h \
+  sdk\src\Tools\ad_parser.h sdk\src\Tools\gatt_client_util.h \
+  sdk\bundles\typical\inc\sm.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h src\profile.h \
+  src\USB_BSP\bsp_usb_hid_iap.h src\USB_BSP\IAP_UserDef.h \
+  src\..\data\gatt.const src\..\data\advertising.adv \
+  src\..\data\advertising.const src\..\data\scan_response.adv \
+  src\..\data\scan_response.const src\..\data\gatt.profile
