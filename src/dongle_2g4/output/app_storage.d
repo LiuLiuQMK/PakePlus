c4f68bfc8a24db66f0a90f4a8c02c2a1e5e86f6c
LiWen <108209252+LiuLiuQMK@users.noreply.github.com>
2025-06-24 19:47:33 +0800

    update src file from pakeplus

---
+++ src/dongle_2g4/output/app_storage.d
@@ -0,0 +1,34 @@
+./output/app_storage.o: src\APP_STORAGE\app_storage.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  sdk\bundles\noos_mini\inc\kv_storage.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  sdk\bundles\noos_mini\inc\platform_api.h \
+  sdk\src\StartUP\ing916\ingsoc.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\.\m-profile\cmsis_armclang_m.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\m-profile\armv7m_mpu.h \
+  sdk\src\StartUP\ing916\..\__ingsoc.h \
+  sdk\src\FWlib\peripheral_pinctrl.h sdk\src\FWlib\peripheral_adc.h \
+  sdk\src\FWlib\peripheral_comparator.h sdk\src\FWlib\peripheral_dma.h \
+  sdk\src\FWlib\peripheral_sysctrl.h sdk\src\FWlib\peripheral_efuse.h \
+  sdk\src\FWlib\peripheral_gpio.h sdk\src\FWlib\peripheral_i2c.h \
+  sdk\src\FWlib\peripheral_i2s.h sdk\src\FWlib\peripheral_ir.h \
+  sdk\src\FWlib\peripheral_pdm.h sdk\src\FWlib\peripheral_pte.h \
+  sdk\src\FWlib\peripheral_pwm.h sdk\src\FWlib\peripheral_qdec.h \
+  sdk\src\FWlib\peripheral_rtc.h sdk\src\FWlib\peripheral_ssp.h \
+  sdk\src\FWlib\peripheral_sysctrl.h sdk\src\FWlib\peripheral_timer.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h \
+  sdk\src\FWlib\peripheral_uart.h sdk\src\FWlib\peripheral_usb.h \
+  sdk\bundles\noos_mini\inc\ll_api.h \
+  sdk\bundles\noos_mini\inc\btstack_util.h \
+  sdk\bundles\noos_mini\inc\bluetooth.h \
+  sdk\bundles\noos_mini\inc\le_device_db.h \
+  sdk\bundles\noos_mini\inc\btstack_util.h \
+  sdk\bundles\noos_mini\inc\bt_types.h \
+  sdk\bundles\noos_mini\inc\btstack_linked_list.h \
+  sdk\bundles\noos_mini\inc\kv_storage.h src\APP_STORAGE\app_storage.h \
+  src\APP_STORAGE\kv_impl.h src\IAP\IAP_FLASH_MAP.H sdk\src\BSP\eflash.h \
+  src\2P4G\app_2g4.h src\2P4G\ing_2p4g.h src\profile.h
