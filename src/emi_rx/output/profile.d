019fc40cfb0689303bf735d6db51f97138dcfaf6
LiWen <108209252+LiuLiuQMK@users.noreply.github.com>
2025-06-24 20:05:47 +0800

    update src file from pakeplus

---
+++ src/emi_rx/output/profile.d
@@ -0,0 +1,38 @@
+./output/profile.o: src\profile.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  sdk\bundles\noos_mini\inc\platform_api.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  sdk\src\StartUP\ing916\ingsoc.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\mpu_armv7.h \
+  sdk\src\StartUP\ing916\..\__ingsoc.h \
+  sdk\src\FWlib\peripheral_pinctrl.h sdk\src\FWlib\peripheral_adc.h \
+  sdk\src\FWlib\peripheral_comparator.h sdk\src\FWlib\peripheral_dma.h \
+  sdk\src\FWlib\peripheral_sysctrl.h sdk\src\FWlib\peripheral_efuse.h \
+  sdk\src\FWlib\peripheral_gpio.h sdk\src\FWlib\peripheral_i2c.h \
+  sdk\src\FWlib\peripheral_i2s.h sdk\src\FWlib\peripheral_ir.h \
+  sdk\src\FWlib\peripheral_pdm.h sdk\src\FWlib\peripheral_pte.h \
+  sdk\src\FWlib\peripheral_pwm.h sdk\src\FWlib\peripheral_qdec.h \
+  sdk\src\FWlib\peripheral_rtc.h sdk\src\FWlib\peripheral_ssp.h \
+  sdk\src\FWlib\peripheral_sysctrl.h sdk\src\FWlib\peripheral_timer.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h \
+  sdk\src\FWlib\peripheral_uart.h sdk\src\FWlib\peripheral_usb.h \
+  sdk\bundles\noos_mini\inc\ll_api.h sdk\bundles\noos_mini\inc\att_db.h \
+  sdk\bundles\noos_mini\inc\bluetooth.h \
+  sdk\bundles\noos_mini\inc\bt_types.h \
+  sdk\bundles\noos_mini\inc\btstack_linked_list.h \
+  sdk\bundles\noos_mini\inc\gap.h \
+  sdk\bundles\noos_mini\inc\btstack_event.h \
+  sdk\src\StartUP\ing916\compiler.h \
+  sdk\bundles\noos_mini\inc\btstack_util.h \
+  sdk\bundles\noos_mini\inc\gap.h \
+  sdk\bundles\noos_mini\inc\btstack_defines.h \
+  sdk\src\Tools\bluetooth_hci.h src\profile.h src\IAP\IAP_916.h \
+  src\IAP\IAP_FLASH_MAP.H sdk\src\BSP\eflash.h src\Ctrl\ctrl.h \
+  src\Ctrl\ctrl_pch.h C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h
