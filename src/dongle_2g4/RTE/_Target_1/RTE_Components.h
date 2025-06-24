0b7a6caeeef40b97d77fb6340e096a2cffbbf4c0
LiWen <108209252+LiuLiuQMK@users.noreply.github.com>
2025-06-24 19:51:56 +0800

    update src file from pakeplus

---
+++ src/dongle_2g4/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'DONGLE_2G4' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
