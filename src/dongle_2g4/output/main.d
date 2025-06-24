48ae69b07baf6877169ce9cb9dfb6cc2aa4246cd
LiWen <108209252+LiuLiuQMK@users.noreply.github.com>
2025-06-24 19:48:56 +0800

    update src file from pakeplus

---
+++ src/dongle_2g4/output/main.d
@@ -0,0 +1,32 @@
+./output/main.o: src\main.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h src\profile.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  sdk\src\StartUP\ing916\ingsoc.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\.\m-profile\cmsis_armclang_m.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Users\rrr\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\m-profile\armv7m_mpu.h \
+  sdk\src\StartUP\ing916\..\__ingsoc.h \
+  sdk\src\FWlib\peripheral_pinctrl.h sdk\src\FWlib\peripheral_adc.h \
+  sdk\src\FWlib\peripheral_comparator.h sdk\src\FWlib\peripheral_dma.h \
+  sdk\src\FWlib\peripheral_sysctrl.h sdk\src\FWlib\peripheral_efuse.h \
+  sdk\src\FWlib\peripheral_gpio.h sdk\src\FWlib\peripheral_i2c.h \
+  sdk\src\FWlib\peripheral_i2s.h sdk\src\FWlib\peripheral_ir.h \
+  sdk\src\FWlib\peripheral_pdm.h sdk\src\FWlib\peripheral_pte.h \
+  sdk\src\FWlib\peripheral_pwm.h sdk\src\FWlib\peripheral_qdec.h \
+  sdk\src\FWlib\peripheral_rtc.h sdk\src\FWlib\peripheral_ssp.h \
+  sdk\src\FWlib\peripheral_sysctrl.h sdk\src\FWlib\peripheral_timer.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h \
+  sdk\src\FWlib\peripheral_uart.h sdk\src\FWlib\peripheral_usb.h \
+  sdk\bundles\noos_mini\inc\platform_api.h \
+  sdk\bundles\noos_mini\inc\ll_api.h sdk\src\Tools\port_gen_os_driver.h \
+  sdk\src\Tools\port_gen_os.h sdk\src\Tools\trace.h \
+  sdk\src\Tools\port_gen_os_driver.h sdk\src\Tools\SEGGER_RTT.h \
+  sdk\src\Tools\SEGGER_RTT_Conf.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdarg.h src\2P4G\ing_2p4g.h \
+  src\uart_console.h src\IAP\IAP_916.h src\IAP\IAP_FLASH_MAP.H \
+  sdk\src\BSP\eflash.h
